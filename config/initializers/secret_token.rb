16688f291754fb42bbc69b849a9ce32bd00a4023
bethanyr <bethany.rentz@hotmail.com>
2013-07-29 09:13:38 -0700

    initial version of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outfitchooser::Application.config.secret_token = '781d1fb7cfc673a6ca87623b6637a9887c3beb838f5d56308a62bb39a9663bb65293843fb9e374724cb23f3ee5d8aa42cb55a091f8cdbd8231baece9ca031e4a'
